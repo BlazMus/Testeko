eec0644f1dd4ccd7f284ca9aaa752cead479289f
BlazMus <rasmuselestedt@gmail.com>
2020-06-05 23:37:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Blazored.Toast;
+
+namespace Iteko
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("app");
+			builder.Services.AddBlazoredToast();
+
+			await builder.Build().RunAsync();
+		}
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddBlazoredToast();
+		}
+	}
+}
